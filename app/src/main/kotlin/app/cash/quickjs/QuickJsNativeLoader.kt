954fd691d434f6e26731d15fa01a683fdd0b2ecb
glitch-228 <66838958+glitch-228@users.noreply.github.com>
2026-09-09 13:13:10 +0300

    Swap the QuickJS engine to quickjs-kt with a cash shim

---
+++ app/src/main/kotlin/app/cash/quickjs/QuickJsNativeLoader.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2026 Kototoro contributors (https://github.com/skepsun/kototoro)
+ * Licensed under Apache License, Version 2.0. Ported for LNReader plugin support.
+ */
+package app.cash.quickjs
+
+internal object QuickJsNativeLoader {
+	fun load() = Unit
+}
